5e031da9cd23763417cfb2430cbda3130b3baa51
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-10-11 11:47:20 +0900

    fix: fix the leak of github_token

--- main.sh
+++ main.sh
@@ -9,14 +9,14 @@ if ! terraform init -input=false; then
 	# but the lock file hasn't been updated
 	github-comment exec -- terraform init -input=false -upgrade
 fi
+
 # https://www.terraform.io/docs/cli/commands/providers/lock.html
 # To prevent a diff when you run `terraform init` at local on Mac
 github-comment exec -- terraform providers lock -platform=linux_amd64 -platform=darwin_amd64
 github-comment exec -- git add .terraform.lock.hcl
 
 if ! git diff --cached --exit-code; then
-	github-comment exec -- \
-		ghcp commit -r "$GITHUB_REPOSITORY" -b "$GITHUB_HEAD_REF" \
+	ghcp commit -r "$GITHUB_REPOSITORY" -b "$GITHUB_HEAD_REF" \
 		-m "chore(terraform-provider): update .terraform.lock.hcl" \
 		-C "$ROOT_DIR" "$WORKING_DIR/.terraform.lock.hcl" \
 		--token "$GITHUB_APP_TOKEN"
