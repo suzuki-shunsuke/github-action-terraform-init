49ed2f5935fedb1947ca741d8f5871963e084297
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-02-26 22:37:54 +0900

    feat: support OpenTofu

--- main.sh
+++ main.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
+CMD=terraform
+if [ "${USE_OPENTOFU}" = true ]; then
+  CMD=tofu
+fi
+
 if [ "$SKIP_PUSH" = "true" ]; then
-  github-comment exec -- terraform init -input=false
+  github-comment exec -- "$CMD" init -input=false
   exit 0
 fi
 
@@ -12,10 +17,10 @@ if [ -f .terraform.lock.hcl ]; then
   exist_lock_file=true
 fi
 
-terraform init -input=false || github-comment exec -- terraform init -input=false -upgrade
+"$CMD" init -input=false || github-comment exec -- "$CMD" init -input=false -upgrade
 
 # shellcheck disable=SC2086
-github-comment exec -- terraform providers lock $PROVIDERS_LOCK_OPTS
+github-comment exec -- "$CMD" providers lock $PROVIDERS_LOCK_OPTS
 
 if [ "$exist_lock_file" = "false" ] || ! git diff --quiet .terraform.lock.hcl; then
 	ghcp commit -r "$GITHUB_REPOSITORY" -b "$GITHUB_HEAD_REF" \
